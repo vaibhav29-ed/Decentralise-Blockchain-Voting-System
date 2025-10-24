fd822b3e411f807b5aaf33cb432bbb628361a5a5
Vaibhav Singh <vaibhavsingh292005@gmail.com>
2025-10-24 05:46:16 +0530

    Implement async deploy function for Voting contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,2 +1,13 @@
-scripts/
-│ ├─ deploy.js
+async function main() {
+const [deployer] = await ethers.getSigners();
+const Voting = await ethers.getContractFactory('Voting');
+const voting = await Voting.deploy();
+await voting.deployed();
+console.log(voting.address);
+}
+
+
+main().catch((error) => {
+console.error(error);
+process.exitCode = 1;
+});
